c98c24540de2552f3b671b3e74f3099d713d7006
Wladimir Guevara <wladige@gmail.com>
2020-10-13 20:31:51 -0300

    terminando formulario login

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,42 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-app class="bg">
+    <v-card width="400" class="mx-auto mt-5">
+      <v-card-title>
+        <h1>Login</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field
+            label="Username"
+            prepend-icon="mdi-account-circle"
+          ></v-text-field>
+          <v-text-field
+            :type="showPassword ? 'text' : 'password'"
+            label="Password"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="success">Register</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="info">Login</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-app>
 </template>
+
+<script>
+export default {};
+data: () => ({
+  showPassword: false,
+});
+</script>
+
+<style lang="scss" scoped>
+.bg {
+  background: grey;
+}
+</style>
