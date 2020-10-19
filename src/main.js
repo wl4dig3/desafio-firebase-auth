73beb30072a9f7efaa5b973ec958f0baaca30c54
Wladimir Guevara <wladige@gmail.com>
2020-10-19 01:41:02 -0300

    detectando usuario registrado

--- src/main.js
+++ src/main.js
@@ -6,9 +6,29 @@ import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+import {auth} from './firebase'
+
+auth.onAuthStateChanged(user=>{
+  if(user){
+    console.log('status usuario logueado',user.email)
+    const detectoUsuario = {
+      email: user.email,
+      uid: user.uid
+    }
+    store.dispatch('detectarUsuario', detectoUsuario)
+
+  }else{
+    console.log(user)
+    store.dispatch('detectarUsuario', user)
+  }
+
+
+  new Vue({
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
+  }).$mount('#app')
+
+})
+
--- src/store/index.js
+++ src/store/index.js
@@ -56,6 +56,9 @@ export default new Vuex.Store({
       .then(()=>{
         router.push('/acceso')
       })
+    },
+    detectarUsuario({commit}, usuario){
+      commit('sertUsuario',usuario)
     }
   }
   
